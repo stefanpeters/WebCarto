9a7ab604fb54f94f825e430b4081067043bb6378
stefanpeters <stefan.peters@unisa.edu.au>
2019-02-08 17:32:28 +1030

    WC_KI
    d

---
+++ pointclouds/KI_FR/cloud.js
@@ -0,0 +1,26 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 140000000,
+    "boundingBox": {
+        "lx": 223608.369,
+        "ly": 6042492.576,
+        "lz": -4.712,
+        "ux": 223903.998,
+        "uy": 6042788.205,
+        "uz": 290.9169999999863
+    },
+    "tightBoundingBox": {
+        "lx": 223624.526,
+        "ly": 6042492.828,
+        "lz": -4.712,
+        "ux": 223903.998,
+        "uy": 6042699.175,
+        "uz": 41.687
+    },
+    "pointAttributes": "LAZ",
+    "spacing": 2.5602221488952638,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
